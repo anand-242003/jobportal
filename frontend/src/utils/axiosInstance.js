36bd40de7a76da4f33f4d8a69cda095a85e36822
Anand Mishra <Anandmishra3001@gmail.com>
2025-11-15 15:09:47 +0530

    Refactor axios instance to dynamically set base URL based on environment and add response interceptor for token refresh handling . Also made a basic dashboard page

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,10 +1,47 @@
 import axios from "axios";
 
+let baseURL = "";
+
+if (process.env.NODE_ENV !== "production") {
+  baseURL = "http://localhost:5001/api";
+} else {
+  baseURL = "https://jobportal-oc40.onrender.com/api";
+}
+
 const axiosInstance = axios.create({
- baseURL: "https://jobportal-oc40.onrender.com/api",
- 
-  withCredentials: true, 
+  baseURL: baseURL,
+  withCredentials: true
 });
 
-export default axiosInstance;
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response; 
+  },
 
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        await axiosInstance.post("/auth/refresh");
+
+        return axiosInstance(originalRequest);
+
+      } catch (refreshError) {
+        console.error("Refresh token failed:", refreshError);
+
+        if (typeof window !== "undefined") {
+          window.location.href = "/auth/login";
+        }
+
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
